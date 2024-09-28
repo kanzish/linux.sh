4c986914c486b95d8ddb44b08c8d1beabf42eb18
kanzish <kanzish@proton.me>
2024-09-27 21:17:55 -0700

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,14 @@
+# Core packages
+apk update
+apk add doas
+apk add nano nano-syntax
+apk add curl tmux
+
+# Git
+apk add git
+git config --global init.defaultBranch main
+
+# Fonts
+apk add terminus-font
+setfont /usr/share/consolefonts/ter-132n.psf.gz
+rc-update add consolefont boot
